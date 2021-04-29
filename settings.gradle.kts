364c59e3d8b7b885d1e46f6a5f533fdcc55665b8
jan <jan@rexster>
2021-04-29 11:47:56 +0200

    fix tests code & dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,15 @@
 rootProject.name = "fritz2-spring-todomvc"
 
+pluginManagement {
+    plugins {
+        id("dev.fritz2.fritz2-gradle") version "0.10"
+    }
+    resolutionStrategy {
+    }
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+    }
+}
+
 include("app", "backend")
